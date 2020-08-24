d981623d23db641f72c4ac06dd6542042edb0b51
Courtney Berry <64494409+courtneyberry27@users.noreply.github.com>
2020-08-24 15:05:39 -0500

    Update UserSignOut.js

--- client/src/components/UserSignOut.js
+++ client/src/components/UserSignOut.js
@@ -5,6 +5,7 @@ import { Redirect } from 'react-router-dom';
  * USER SIGN OUT CLASS
  *************************/
 export default ({ context }) => {
+  //use signOut effect
   useEffect(() =>context.actions.signOut());
 
   return (
