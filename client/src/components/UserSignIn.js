fe0ccde3bcf8187377f6101909184f7db6bd18ec
Courtney Berry <64494409+courtneyberry27@users.noreply.github.com>
2020-08-24 15:05:00 -0500

    Update UserSignIn.js

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -63,6 +63,7 @@ export default class UserSignIn extends Component {
     const name = event.target.name;
     const value = event.target.value;
 
+    //changes old value to new 
     this.setState(() => {
       return {
         [name]: value
@@ -79,11 +80,13 @@ export default class UserSignIn extends Component {
     const { emailAddress, password} = this.state;
     context.actions.signIn(emailAddress, password)
     .then( user => {
+      //if user doesn't exist
       if (user === null) {
         this.setState(() => {
           //UNSUCCESSFUL
           return { errors: [ 'Sign-in was unsuccessful' ] }
         })
+      //if user does exist
       } else {
         //SUCCESSFUL
         this.props.history.push(from)
